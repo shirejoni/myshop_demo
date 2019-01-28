6a1cfb2641025b1039f04bfa7c0e2d822dd2cc5b
Hashna <hashna@iran.ir>
2019-01-28 15:16:07 +0330

    Require Config/constant.php file

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ define("DS", DIRECTORY_SEPARATOR);
 define("ROOT_PATH", dirname(__DIR__));
 define("APP_PATH", ROOT_PATH . DS . 'application');
 
+require_once APP_PATH . DS . 'config' . DS . 'constant.php';
+
 require_once ROOT_PATH . DS . 'vendor' . DS . "autoload.php";
 
 
