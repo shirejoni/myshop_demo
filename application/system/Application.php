9d5cf17a45ce2123f7dcd0e6ade254b37c667ec0
Hashna <hashna@iran.ir>
2019-01-28 15:04:05 +0330

    create Application class and instantiate it

---
+++ application/system/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\System;
+
+
+class Application {
+
+
+    /**
+     * Application constructor.
+     */
+    public function __construct()
+    {
+        echo "Hello World";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,3 +4,10 @@ define("ROOT_PATH", dirname(__DIR__));
 define("APP_PATH", ROOT_PATH . DS . 'application');
 
 require_once ROOT_PATH . DS . 'vendor' . DS . "autoload.php";
+
+
+try {
+    $application = new \App\System\Application();
+}catch (Exception $e) {
+    echo $e->getMessage();
+}
