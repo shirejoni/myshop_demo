33c01f1bb2cd37b79b8ed4534459a5ea9464aa4a
Hashna <hashna@iran.ir>
2019-01-28 14:46:36 +0330

    adding constant.php: for define some common paths and info

---
+++ application/config/constant.php
@@ -0,0 +1,15 @@
+<?php
+
+define("WEB_PATH", APP_PATH . DS . 'web');
+define("ADMIN_PATH", APP_PATH . DS . 'admin');
+define("MODEL_PATH", APP_PATH . DS . 'model');
+define("LANGUAGE_PATH", APP_PATH . DS . 'language');
+define("SYSTEM_PATH", APP_PATH . DS . 'system');
+
+define("DB_NAME", "demoshop");
+define("DB_USER", "root");
+define("DB_SERVER", "localhost");
+define("DB_PASSWORD", "");
+
+define("URL", "http://myshopdemo.test/");
+define("LANGUAGE_DEFAULT_CODE", "fa");
