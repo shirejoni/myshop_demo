fdc93819db746961caf8c2f1e2b4209489a66b24
Hashna <hashna@iran.ir>
2019-01-29 14:24:49 +0330

    Adding Router class for Route the url and call actions

---
+++ application/lib/Router.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Lib;
+
+use App\Lib\Action;
+use App\Lib\Registry;
+
+class Router {
+    private $registry;
+    private $routes = [];
+    private $baseRoute = '';
+    private $preRoutes = [];
+
+
+    /**
+     * Router constructor.
+     * @param Registry $registry
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    public function Dispatch()
+    {
+
+        if(count($this->preRoutes)) {
+            foreach ($this->preRoutes as $preRoute) {
+                /** @var Action $preAction */
+                $preAction = $preRoute['action'];
+                $result = $preAction->execute($this->registry);
+                if($result instanceof Action) {
+                    $action = $result;
+                }
+            }
+        }
+        if(empty($action)) {
+            $uri = $this->registry->Application->getUri();
+            if($uri == "" || $uri == "/") {
+                $uri = 'home/index';
+            }
+            $action = new Action($uri);
+            if(!$action->isStatus()) {
+                $action = new Action('error/notFound',"web");
+            }
+        }
+        $action->execute($this->registry, array(
+            'error_route'   => 'error/notFound',
+            'error_pre_route' => 'web'
+        ));
+    }
+
+
+    /**
+     * @param Action $action
+     * @param array $params
+     * @param bool $mainOutPut
+     */
+    public function addPreRoute(Action $action, array $params = array(), $mainOutPut = false)
+    {
+        $this->preRoutes[] = array(
+            'action'    => $action,
+            'params'    => $params,
+            'mainOutPut' => $mainOutPut
+        );
+    }
+
+}
