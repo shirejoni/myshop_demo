3a273c7a9193257bcda77553cae0dcc2ed677f13
Hashna <hashna@iran.ir>
2019-01-29 15:00:02 +0330

    Adding Admin/Home Controller

---
+++ application/admin/controller/home.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Admin\Controller;
+
+use App\System\Controller;
+
+class ControllerHome extends Controller {
+
+    public function index() {
+        echo "<b>ControllerHome</b>/index : 8 ";
+    }
+
+
+}
