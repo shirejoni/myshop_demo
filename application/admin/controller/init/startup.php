d084a922f9d0563b63dcb88c971c1649c212b968
Hashna <hashna@iran.ir>
2019-01-30 08:41:40 +0330

    Add Startup PreAction Controller

---
+++ application/admin/controller/init/startup.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Admin\Controller;
+
+use App\Lib\Config;
+use App\Lib\Database;
+use App\System\Controller;
+
+/**
+ * @property Database Database
+ * @property Config Config
+ */
+class ControllerInitStartup extends Controller {
+
+    public function init() {
+        $configs = $this->Database->getRows("SELECT * FROM config");
+        $Config = $this->Config;
+        foreach ($configs as $config) {
+            if($config['serialized'] == 0) {
+                $Config->set($config['key'], $config['value']);
+            }else {
+
+                $Config->set($config['key'], unserialize($config['value']));
+            }
+        }
+    }
+}
