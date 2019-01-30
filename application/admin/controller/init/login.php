6bb0c6682ddbf33eccf4596f3ae660c5aff00890
Hashna <hashna@iran.ir>
2019-01-30 08:50:49 +0330

    Add Login PreAction Controller

---
+++ application/admin/controller/init/login.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Admin\Controller;
+
+use App\System\Controller;
+
+class ControllerInitLogin extends Controller {
+    public function index() {
+        echo "ControllerInitLogin index 9 ";
+    }
+
+}
