3063560060b3966727b050d5af20652ce7ed2f45
Hashna <hashna@iran.ir>
2019-01-31 10:17:46 +0330

    Add Multi Language Feature to project

---
+++ application/language/en/admin/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+/*
+ * Error
+ * */
+$_['error_email_invalid'] = "Email is invalid!";
+$_['error_password_invalid'] = "Password is invalid!";
---
+++ application/model/Language.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Model;
+
+use App\Lib\Database;
+use App\Lib\Registry;
+
+class Language {
+    private $db;
+    private $defaultLanguageDir = DEFAULT_LANGUAGE_DIR ;
+    private $defaultLanguageCode = DEFAULT_LANGUAGE_CODE;
+    private $languages = [];
+    private $languageDir;
+    private $languageID;
+    private $languageCode;
+    private $data = [];
+    private $registry;
+
+    /**
+     * Language constructor.
+     * @param Registry $registry
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->registry = $registry;
+        /** @var Database $db */
+        $db = $this->registry->Database;
+        $this->db = $db;
+        $languagesResults = $this->db->getRows("SELECT * FROM langauge");
+        foreach ($languagesResults as $languagesResult) {
+            $this->languages[$languagesResult['code']] = $languagesResult;
+        }
+        $this->languageID = $this->languages[$this->defaultLanguageCode]['language_id'];
+        $this->languageDir = $this->languages[$this->defaultLanguageCode]['code'];
+        $this->languageCode = $this->languages[$this->defaultLanguageCode]['code'];
+    }
+
+    public function setLanguageByID($languageID) {
+        foreach ($this->languages as $language) {
+            if($language['language_id'] == $languageID) {
+                $this->languageDir = $language['code'];
+                $this->languageCode = $language['code'];
+                $this->languageID = $language['language_id'];
+                break;
+            }
+        }
+    }
+
+
+    /**
+     *
+     *
+     * @param	string	$key
+     *
+     * @return	string
+     */
+    public function get($key) {
+        return (isset($this->data[$key]) ? $this->data[$key] : $key);
+    }
+
+    public function set($key, $value) {
+        $this->data[$key] = $value;
+    }
+
+    /**
+     *
+     *
+     * @return	array
+     */
+    public function all() {
+        return $this->data;
+    }
+
+    /**
+     *
+     *
+     * @param	string	$filename
+     * @param	string	$key
+     *
+     * @return	array
+     */
+    public function load($filename, $key = '') {
+        if (!$key) {
+            $_ = array();
+
+            $file = LANGUAGE_PATH . DS . $this->defaultLanguageDir . DS . $filename . '.php';
+
+            if (is_file($file)) {
+                require($file);
+            }
+
+            $file = LANGUAGE_PATH . DS . $this->languageDir . DS . $filename . '.php';
+
+            if (is_file($file)) {
+                require($file);
+            }
+
+            $this->data = array_merge($this->data, $_);
+        } else {
+            // Put the language into a sub key
+            $this->data[$key] = new Language($this->registry);
+            $this->data[$key]->load($filename);
+        }
+
+        return $this->data;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDefaultLanguageCode()
+    {
+        return $this->defaultLanguageCode;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLanguages(): array
+    {
+        return $this->languages;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguageID()
+    {
+        return $this->languageID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguageCode()
+    {
+        return $this->languageCode;
+    }
+
+    public function getLanguageByCode($code) {
+        if(!empty($this->languages[$code])) {
+            return $this->languages[$code];
+        }else {
+            return false;
+        }
+    }
+
+}
