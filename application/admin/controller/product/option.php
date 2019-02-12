6c8a1230ebe7fa311ac4d5aaafc4a318c459b16c
Hashna <hashna@iran.ir>
2019-02-12 17:32:59 +0330

    Add Option to the project

---
+++ application/admin/controller/product/option.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace App\Admin\Controller;
+
+use App\Lib\Config;
+use App\Lib\Database;
+use App\Lib\Request;
+use App\Lib\Response;
+use App\Model\Language;
+use App\Model\Option;
+use App\System\Controller;
+
+/**
+ * @property Response Response
+ * @property Request Request
+ * @property Database Database
+ * @property Option Option
+ * @property Language Language
+ * @property Config Config
+ */
+class ControllerProductOption extends Controller {
+
+    public function index() {
+        $data = [];
+        $language_id = $this->Language->getLanguageID();
+        /** @var Option $Option */
+        $Option = $this->load("Option", $this->registry);
+        $data['Options'] = $Option->getOptionGroups(array(
+            'language_id'   => $language_id
+        ));
+        $Language = $this->Language;
+        $data['Languages'] = $Language->getLanguages();
+        $data['LanguageDefaultID'] = $Language->getDefaultLanguageID();
+        $this->Response->setOutPut($this->render('product/option/index', $data));
+    }
+
+    public function add() {
+        $data = array();
+        $error = false;
+        $messages = [];
+
+        if(!empty($this->Request->post['option-post'])) {
+
+            $Language = $this->Language;
+            $languages = $Language->getLanguages();
+            $languageDefaultID = $Language->getDefaultLanguageID();
+            $options = [];
+            foreach ($languages as $language) {
+                if(!empty($this->Request->post['option-group-name-' . $language['language_id']])) {
+                    $data['option_group_names'][$language['language_id']] = $this->Request->post['option-group-name-' . $language['language_id']];
+                }
+                if(isset($this->Request->post['option-name-' . $language['language_id']])) {
+                    foreach ($this->Request->post['option-name-' . $language['language_id']] as $sort_order => $filter_name) {
+                        $options[$sort_order][$language['language_id']] = $filter_name;
+                    }
+                }
+            }
+            unset($options[0]);
+            foreach ($options as $option) {
+                if(!isset($option[$languageDefaultID])) {
+                    $error = true;
+                    $messages[] = $this->Language->get('error_option_not_enough');
+                }
+            }
+            foreach ($options as $sort_order => $option) {
+                if(isset($this->Request->post['option-image'][$sort_order])) {
+                    $data['option_image'][$sort_order] = $this->Request->post['option-image'][$sort_order];
+                }else {
+                    $data['option_image'][$sort_order] = '';
+                }
+            }
+            $data['options'] = $options;
+            if(empty($data['option_group_names'][$languageDefaultID])) {
+                $error = true;
+                $messages[] = $this->Language->get('error_option_name');
+            }
+
+            if(!empty($this->Request->post['option-sort-order'])) {
+                $data['sort_order'] = (int) $this->Request->post['option-sort-order'];
+            }else {
+                $data['sort_order'] = 0;
+            }
+            if(!empty($this->Request->post['option-type']) && in_array($this->Request->post['option-type'], $this->Config->get('option_type'))) {
+                $data['type'] = $this->Request->post['option-type'];
+            }else {
+                $error = true;
+                $messages[] = $this->Language->get('error_option_type_select');
+            }
+
+            $json = [];
+            if(!$error) {
+                /** @var Option $Option */
+                $Option = $this->load("Option", $this->registry);
+                if($data['sort_order'] == 0) {
+                    $rows = $Option->getOptionGroups(array(
+                        'sort'  => 'sort_order',
+                        'order' => 'DESC',
+                        'language_id'   => $languageDefaultID
+                    ));
+                    $oldSortOrder = count($rows) > 0 ? $rows[0]['sort_order'] : 0;
+                    $data['sort_order'] = $oldSortOrder + 1;
+                }
+                $option_id = $Option->insertOptionGroup($data);
+                $Option->insertOptionValues($option_id, $data);
+                $json['status'] = 1;
+                $this->Response->endResponse();
+                $json['process_time'] = $this->Response->getProcessTime();
+                $json['messages'] = [$this->Language->get('message_success_done')];
+                $json['redirect'] = ADMIN_URL . "product/option/index?token=" . $_SESSION['token'];
+                $json['data'] = $data;
+            }else {
+                $json['status'] = 0;
+                $json['messages'] = $messages;
+            }
+            $this->Response->setOutPut(json_encode($json));
+        }else {
+            $Language = $this->Language;
+            $data['Languages'] = $Language->getLanguages();
+            $data['OptionTypes'] = $this->Config->get('option_type');
+            $data['LanguageDefaultID'] = $Language->getDefaultLanguageID();
+            $this->Response->setOutPut($this->render('product/option/add', $data));
+        }
+    }
+
+    public function delete() {
+        if(!empty($this->Request->post['options_id'])) {
+            $json = [];
+            /** @var Option $Option */
+            $Option = $this->load("Option", $this->registry);
+            $error = false;
+            $this->Database->db->beginTransaction();
+            foreach ($this->Request->post['options_id'] as $option_id) {
+                $option = $Option->getOptionByID((int) $option_id);
+                if((int) $option_id && $option) {
+                    $Option->deleteOption($option_id);
+                }else {
+                    $error = true;
+                }
+            }
+            if($error) {
+                $this->Database->db->rollBack();
+                $json['status'] = 0;
+                $json['messages'] = [$this->Language->get('error_done')];
+            }else {
+                $this->Database->db->commit();
+                $json['status'] = 1;
+                $json['messages'] = [$this->Language->get('message_success_done')];
+                $data = [];
+                $language_id = $this->Language->getLanguageID();
+                $data['Options'] = $Option->getOptionGroups(array(
+                    'language_id'   => $language_id
+                ));
+                $Language = $this->Language;
+                $data['Languages'] = $Language->getLanguages();
+                $data['LanguageDefaultID'] = $Language->getDefaultLanguageID();
+                $json['data'] = $this->render("product/option/options_table", $data);
+            }
+
+            $this->Response->setOutPut(json_encode($json));
+            return;
+        }
+        return new Action("error/notFound", 'web');
+    }
+
+    public function edit() {
+        $data = array();
+        $error = false;
+        $messages = [];
+        if(isset($this->Request->get[0])) {
+            $option_id = (int) $this->Request->get[0];
+            /** @var Option $Option */
+            $Option = $this->load("Option", $this->registry);
+            $optionTotal = $Option->getOptionByID($option_id, "all");
+            $optionInfo = [];
+            $Language = $this->Language;
+            foreach ($optionTotal as $option) {
+                $optionInfo['option_group_names'][$option['language_id']] = $option['name'];
+            }
+            $optionInfo['sort_order'] = $optionTotal[0]['sort_order'];
+            $optionInfo['type'] = $optionTotal[0]['option_type'];
+            $optionInfo['option_id'] = $optionTotal[0]['option_id'];
+
+            foreach ($Option->getOptionValues() as $optionValue) {
+                $optionInfo['options'][$optionValue['language_id']][$optionValue['option_value_id']] = $optionValue;
+            }
+
+            if($option_id && $optionInfo) {
+                if(!empty($this->Request->post['option-post'])) {
+
+                    $Language = $this->Language;
+                    $languages = $Language->getLanguages();
+                    $languageDefaultID = $Language->getDefaultLanguageID();
+                    $options = [];
+                    foreach ($languages as $language) {
+                        if(!empty($this->Request->post['option-group-name-' . $language['language_id']])) {
+                            $data['option_group_names'][$language['language_id']] = $this->Request->post['option-group-name-' . $language['language_id']];
+                        }
+                        if(isset($this->Request->post['option-name-' . $language['language_id']])) {
+                            foreach ($this->Request->post['option-name-' . $language['language_id']] as $sort_order => $filter_name) {
+                                $options[$sort_order][$language['language_id']] = $filter_name;
+                            }
+                        }
+                    }
+                    unset($options[0]);
+                    foreach ($options as $option) {
+                        if(!isset($option[$languageDefaultID])) {
+                            $error = true;
+                            $messages[] = $this->Language->get('error_option_not_enough');
+                        }
+                    }
+                    foreach ($options as $sort_order => $option) {
+                        if(isset($this->Request->post['option-image'][$sort_order])) {
+                            $data['option_image'][$sort_order] = $this->Request->post['option-image'][$sort_order];
+                        }else {
+                            $data['option_image'][$sort_order] = '';
+                        }
+                    }
+                    $data['options'] = $options;
+                    if(empty($data['option_group_names'][$languageDefaultID])) {
+                        $error = true;
+                        $messages[] = $this->Language->get('error_option_name');
+                    }
+
+                    if(!empty($this->Request->post['option-sort-order'])) {
+                        $data['sort_order'] = (int) $this->Request->post['option-sort-order'];
+                    }else {
+                        $data['sort_order'] = 0;
+                    }
+                    if(!empty($this->Request->post['option-type']) && in_array($this->Request->post['option-type'], $this->Config->get('option_type'))) {
+                        $data['type'] = $this->Request->post['option-type'];
+                    }else {
+                        $error = true;
+                        $messages[] = $this->Language->get('error_option_type_select');
+                    }
+
+                    $json = [];
+                    if(!$error) {
+                        $delete = [];
+                        $add = [];
+                        if($data['sort_order'] == 0) {
+                            $rows = $Option->getOptionGroups(array(
+                                'sort'  => 'sort_order',
+                                'order' => 'DESC',
+                                'language_id'   => $languageDefaultID
+                            ));
+                            $oldSortOrder = count($rows) > 0 ? $rows[0]['sort_order'] : 0;
+                            $data['sort_order'] = $oldSortOrder + 1;
+                        }
+
+                        if($optionInfo['sort_order'] == $data['sort_order']) {
+                            unset($data['sort_order']);
+                        }
+                        foreach ($languages as $language) {
+                            if(isset($data['option_group_names'][$language['language_id']])
+                                && isset($optionInfo['option_group_names'][$language['language_id']])
+                                && $data['option_group_names'][$language['language_id']] == $optionInfo['option_group_names'][$language['language_id']] ) {
+                                unset($data['option_group_names'][$language['language_id']]);
+                            }else if(isset($optionInfo['option_group_names'][$language['language_id']])
+                                && !isset($data['option_group_names'][$language['language_id']])) {
+                                $delete['option_group_names'][$language['language_id']] = $optionInfo['manufacturer_id'];
+                            }else if (!isset($optionInfo['option_group_names'][$language['language_id']])
+                                && isset($data['option_group_names'][$language['language_id']])) {
+                                $add['option_group_names'][$language['language_id']] =  $data['option_group_names'][$language['language_id']];
+                                unset($data['option_group_names'][$language['language_id']]);
+                            }
+                        }
+                        if(count($data['option_group_names']) == 0) {
+                            unset($data['option_group_names']);
+                        }
+                        if(count($data) > 0) {
+                            $Option->editOption($optionInfo['option_id'], $data);
+                        }
+                        if(count($add) > 0) {
+                            $Option->insertOptionGroup($add, $optionInfo['option_id']);
+                        }
+                        if(count($delete) > 0) {
+                            $Option->deleteOption($optionInfo['option_id'], $delete);
+                        }
+
+                        $json['status'] = 1;
+                        $json['messages'] = [$this->Language->get('message_success_done')];
+                        $json['redirect'] = ADMIN_URL . "product/option/index?token=" . $_SESSION['token'];
+                    }else {
+                        $json['status'] = 0;
+                        $json['messages'] = $messages;
+                    }
+                    $this->Response->setOutPut(json_encode($json));
+                    return;
+                }else {
+
+                    $data['Languages'] = $Language->getLanguages();
+                    $data['OptionLanguages'] = [];
+                    foreach ($Language->getLanguages() as $language) {
+                        $data['OptionLanguages'][] = array(
+                            'language_id' => $language['language_id'],
+                            'language_name' => $language['name'],
+                            'language_code' => $language['code'],
+                            'option_sort_order' => $optionInfo['sort_order'],
+                            'option_id' => $optionInfo['option_id'],
+                            'options' => isset($optionInfo['options'][$language['language_id']]) ? $optionInfo['options'][$language['language_id']] : '',
+                            'option_name' => isset($optionInfo['option_group_names'][$language['language_id']]) ? $optionInfo['option_group_names'][$language['language_id']] : '',
+                        );
+                    }
+                    $data['Options'] = $optionInfo;
+                    $data['OptionTypes'] = $this->Config->get('option_type');
+                    $data['LanguageDefaultID'] = $Language->getDefaultLanguageID();
+                    $this->Response->setOutPut($this->render('product/option/edit', $data));
+                    return;
+                }
+            }
+        }
+        return new Action('error/notFound', 'web');
+    }
+
+}
---
+++ application/model/Option.php
@@ -0,0 +1,268 @@
+<?php
+
+
+namespace App\Model;
+
+
+use App\Lib\Database;
+use App\System\Model;
+
+/**
+ * @property Database Database
+ * @property Language Language
+ */
+class Option extends Model
+{
+    private $option_id;
+    private $option_sort_order;
+    private $language_id;
+    private $option_name;
+    private $rows = [];
+    private $optionValues;
+
+    public function getOptionGroups($data = []) {
+        $data['sort'] = isset($data['sort']) ? $data['sort'] : '';
+        $data['order'] = isset($data['order']) ? strtoupper($data['order']) : 'ASC';
+        $data['language_id'] = isset($data['language_id']) ? $data['language_id'] : $this->Language->getLanguageID();
+
+        $sql = "SELECT * FROM `option` o JOIN `option_language` ol on o.option_id = ol.option_id WHERE
+        ol.language_id = :lID ";
+        $sort_data = array(
+            'name',
+            'sort_order'
+        );
+
+        if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+            $sql .= " ORDER BY " . $data['sort'];
+        }else {
+            $data['sort'] = '';
+            $sql .= " ORDER BY o.option_id";
+        }
+
+        if (isset($data['order']) && ($data['order'] == 'DESC')) {
+            $sql .= " DESC";
+        } else {
+            $sql .= " ASC";
+        }
+
+        if (isset($data['start']) || isset($data['limit'])) {
+            if ($data['start'] < 0) {
+                $data['start'] = 0;
+            }
+
+            if ($data['limit'] < 1) {
+                $data['limit'] = 20;
+            }
+
+            $sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+        }
+        $this->Database->query($sql, array(
+            'lID'   => $data['language_id'],
+        ));
+        $rows = $this->Database->getRows();
+
+
+        return $rows;
+    }
+
+    public function getOptionByID($option_id, $lID = null) {
+        $language_id = $this->Language->getLanguageID();
+        if($lID != null && $lID != "all") {
+            $language_id = $lID;
+        }
+        if($lID != "all") {
+            $this->Database->query("SELECT * FROM `option` o JOIN option_language ol on o.option_id = ol.option_id
+            WHERE ol.language_id = :lID AND o.option_id = :oID", array(
+                'lID'   => $language_id,
+                'oID'   => $option_id
+            ));
+            $row = $this->Database->getRow();
+            $this->option_id = $row['option_id'];
+            $this->option_sort_order = $row['sort_order'];
+            $this->language_id = $row['language_id'];
+            $this->option_name = $row['name'];
+            $this->rows = [];
+            $this->rows[0] = $row;
+            $this->Database->query("SELECT * FROM option_value ov JOIN option_value_language ovl on ov.option_value_id = ovl.option_value_id WHERE
+            ov.option_id = :oID AND ovl.language_id = :lID", array(
+                'oID'  => $option_id,
+                'lID'   => $language_id
+            ));
+            $this->optionValues = $this->Database->getRows();
+            $row['options'] = $this->optionValues;
+            return $row;
+        }else {
+            $this->Database->query("SELECT * FROM `option` o JOIN option_language ol on o.option_id = ol.option_id
+            WHERE o.option_id = :oID", array(
+                'oID'   => $option_id
+            ));
+            $rows = $this->Database->getRows();
+            $this->option_id = $rows[0]['option_id'];
+            $this->option_sort_order = $rows[0]['sort_order'];
+            $this->rows = $rows;
+            $this->Database->query("SELECT * FROM option_value ov JOIN option_value_language ovl on ov.option_value_id = ovl.option_value_id WHERE
+            ov.option_id = :oID", array(
+                'oID'  => $option_id
+            ));
+            $this->optionValues = $this->Database->getRows();
+            $this->rows['options'] = $this->optionValues;
+            return $rows;
+        }
+    }
+
+    public function insertOptionGroup($data, $option_id = null) {
+        if(!$option_id) {
+            $this->Database->query("INSERT INTO `option` (option_type, sort_order) VALUES (:oType,:oSortOrder)", array(
+                'oSortOrder'=> $data['sort_order'],
+                'oType'=> $data['type']
+            ));
+            $option_id = $this->Database->insertId();
+        }
+        foreach ($data['option_group_names'] as $language_id => $option_name) {
+            $this->Database->query("INSERT INTO option_language (option_id, language_id, name) VALUES (:oID, :lID, :oName) ", array(
+                'oID'   => $option_id,
+                'lID'   => $language_id,
+                'oName' => $option_name
+            ));
+        }
+
+        return $option_id;
+    }
+
+    public function insertOptionValues($option_id, $data) {
+        foreach ($data['options'] as $sort_order => $item) {
+            $this->Database->query("INSERT INTO option_value (option_id, image, sort_order) VALUES (:oID, :oImage, :oSortOrder)", array(
+                'oID'  => $option_id,
+                'oImage'    => $data['option_image'][$sort_order],
+                'oSortOrder'   => $sort_order
+            ));
+            $option_value_id = $this->Database->insertId();
+            foreach ($item as $language_id => $filter_name) {
+                $this->Database->query("INSERT INTO option_value_language (option_value_id, language_id, name) VALUES (:oVID, :lID, :oVName)", array(
+                    'oVID'   => $option_value_id,
+                    'lID'   => $language_id,
+                    'oVName' => $filter_name
+                ));
+
+            }
+        }
+    }
+
+    public function deleteOption($option_id, $data = []) {
+        if(isset($data['option_group_names']) && count($data['option_group_names']) > 0) {
+            foreach ($data['option_group_names'] as $language_id => $option_name) {
+                $this->Database->query("DELETE FROM option_language WHERE language_id = :lID AND option_id = :oID ", array(
+                    'lID'   => $language_id,
+                    'oID'   => $option_id
+                ));
+            }
+        }else {
+            $this->Database->query("DELETE FROM `option` WHERE option_id = :oID ", array(
+                'oID'   => $option_id
+            ));
+        }
+
+        return $this->Database->numRows();
+    }
+
+    public function editOption($option_id, $data) {
+        if(count($data) > 0) {
+            $sql = "UPDATE `option` SET ";
+            $params = [];
+            $query = [];
+
+            if(isset($data['sort_order'])) {
+                $query[] = "sort_order = :oSortOrder ";
+                $params['oSortOrder'] = $data['sort_order'];
+            }
+
+            $sql .= implode(" , ", $query);
+            $sql .= " WHERE option_id = :oID ";
+            $params['oID'] = $option_id;
+            if(count($query) > 0) {
+                $this->Database->query($sql, $params);
+            }
+            if(isset($data['option_group_names'])) {
+                foreach ($data['option_group_names'] as $language_id => $option_group_name) {
+                    $this->Database->query("UPDATE option_language SET name = :oName WHERE option_id = :oID AND language_id = :lID ", array(
+                        'oID'   => $option_id,
+                        'oName' => $option_group_name,
+                        'lID'   => $language_id
+                    ));
+                }
+            }
+            $this->Database->query("DELETE FROM option_value WHERE option_id = :oID", array(
+                'oID'  => $option_id
+            ));
+            foreach ($data['options'] as $sort_order => $item) {
+                $this->Database->query("INSERT INTO option_value (option_id, image, sort_order) VALUES (:oID, :oImage, :oSortOrder)", array(
+                    'oID'  => $option_id,
+                    'oImage'    => $data['option_image'][$sort_order],
+                    'oSortOrder'   => $sort_order
+                ));
+                $option_value_id = $this->Database->insertId();
+                foreach ($item as $language_id => $filter_name) {
+                    $this->Database->query("INSERT INTO option_value_language (option_value_id, language_id, name) VALUES (:oVID, :lID, :oVName)", array(
+                        'oVID'   => $option_value_id,
+                        'lID'   => $language_id,
+                        'oVName' => $filter_name
+                    ));
+
+                }
+            }
+
+            return $this->Database->numRows() > 0 ? true : false;
+        }
+        return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOptionId()
+    {
+        return $this->option_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOptionSortOrder()
+    {
+        return $this->option_sort_order;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguageId()
+    {
+        return $this->language_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOptionName()
+    {
+        return $this->option_name;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRows(): array
+    {
+        return $this->rows;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOptionValues()
+    {
+        return $this->optionValues;
+    }
+
+
+}
