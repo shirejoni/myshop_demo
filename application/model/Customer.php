ae3c7b8f8cfee67928198195bf4d929e8dc689be
shirejoni <hosseinshirjoni@chmail.ir>
2019-04-30 20:04:27 +0330

    Add: customer account edit page and Controller

--- application/model/Customer.php
+++ application/model/Customer.php
@@ -75,6 +75,30 @@ class Customer extends Model
         return false;
     }
 
+    public function getCustomerByID($customer_id) {
+        $this->Database->query("SELECT * FROM customer WHERE customer_id = :customer_id", array(
+            'customer_id' => $customer_id
+        ));
+        if($this->Database->hasRows()) {
+            $result = $this->Database->getRow();
+            $this->customer_id = $result['customer_id'];
+            $this->first_name = $result['first_name'];
+            $this->last_name = $result['last_name'];
+            $this->language_id = $result['language_id'];
+            $this->email = $result['email'];
+            $this->mobile = $result['mobile'];
+            $this->cart = json_decode($result['cart']);
+            $this->wishlist = json_decode($result['wishlist']);
+            $this->newsletter = $result['newsletter'];
+            $this->status = $result['status'];
+            $this->token = $result['token'];
+            $this->code = $result['code'];
+            $this->address_id = $result['address_id'];
+            return $result;
+        }
+        return false;
+    }
+
     public function insertCustomer($data) {
         $this->Database->query("INSERT INTO customer (language_id, first_name, last_name, email, mobile, password, cart, wishlist, newsletter, address_id, status, date_added) VALUES 
         (:lID, :cFName, :cLName, :cEmail, :cMobile, :cPassword, :cCart, :cWishlist, :cNews, :cAID, :cStatus, :cDateAdded)", array(
@@ -100,7 +124,7 @@ class Customer extends Model
             $_SESSION['customer'] = [];
             session_regenerate_id();
             $_SESSION['customer'] = array(
-                'user_id'   => $this->customer_id,
+                'customer_id'   => $this->customer_id,
                 'email'     => $this->email,
                 'status'        => $this->status,
             );
@@ -109,6 +133,67 @@ class Customer extends Model
             throw new \Exception("Customer->Login() : You Should First Get User info from Database after that login it");
         }
 
+    }
+
+    public function edit($customer_id, $data) {
+        $sql = "UPDATE customer SET ";
+        $params = [];
+        $query = [];
+
+        if(isset($data['language_id'])) {
+            $query[] = "language_id = :language_id ";
+            $params['language_id'] = $data['language_id'];
+        }
+        if(isset($data['first_name'])) {
+            $query[] = "first_name = :cFName ";
+            $params['cFName'] = $data['first_name'];
+        }
+        if(isset($data['last_name'])) {
+            $query[] = "last_name = :cLName ";
+            $params['cLName'] = $data['last_name'];
+        }
+        if(isset($data['mobile'])) {
+            $query[] = "mobile = :cMobile ";
+            $params['cMobile'] = $data['mobile'];
+        }
+        if(isset($data['password'])) {
+            $query[] = "password = :cPassword ";
+            $params['cPassword'] = $data['password'];
+        }
+        if(isset($data['cart'])) {
+            $query[] = "cart = :cCart ";
+            $params['cCart'] = json_encode($data['cart']);
+        }
+        if(isset($data['wishlist'])) {
+            $query[] = "wishlist = :cWishlist ";
+            $params['cWishlist'] = json_encode($data['wishlist']);
+        }
+        if(isset($data['newsletter'])) {
+            $query[] = "newsletter = :cNews ";
+            $params['cNews'] = $data['newsletter'];
+        }
+        if(isset($data['address_id'])) {
+            $query[] = "address_id = :cAID ";
+            $params['cAID'] = $data['address_id'];
+        }
+        if(isset($data['status'])) {
+            $query[] = "status = :cStatus ";
+            $params['cStatus'] = $data['status'];
+        }
+        if(isset($data['token'])) {
+            $query[] = "token = :cToken ";
+            $params['cToken'] = $data['token'];
+        }
+        if(isset($data['code'])) {
+            $query[] = "code = :cCode ";
+            $params['cCode'] = $data['code'];
+        }
+        $sql .= implode(" , ", $query);
+        $sql .= " WHERE customer_id = :cID ";
+        $params['cID'] = $customer_id;
+        if(count($query) > 0) {
+            $this->Database->query($sql, $params);
+        }
     }
 
 }
--- application/web/controller/user.php
+++ application/web/controller/user.php
@@ -2,16 +2,72 @@
 
 namespace App\Web\Controller;
 
+use App\Lib\Action;
+use App\Lib\Request;
 use App\Lib\Response;
+use App\model\Customer;
+use App\Model\Language;
 use App\System\Controller;
 
 /**
  * @property Response Response
+ * @property Request Request
+ * @property Language Language
  */
 class ControllerUser extends Controller {
 
     public function index() {
         $this->Response->setOutPut($this->render('user/index'));
+    }
+
+    public function edit() {
+        $data = [];
+        $messages = [];
+        $error = false;
+        $customer_id = $_SESSION['customer']['customer_id'];
+        /** @var Customer $Customer */
+        $Customer = $this->load("Customer", $this->registry);
+        $customer = $Customer->getCustomerByID($customer_id);
+        if($customer) {
+            if(isset($this->Request->post['customer-post'])) {
+                if(empty($this->Request->post['customer-first-name'])) {
+                    $error = true;
+                    $messages[] = $this->Language->get("error_first_name_empty");
+                }else if($customer['first_name'] != $this->Request->post['customer-first-name']) {
+                    $data['first_name'] = $this->Request->post['customer-first-name'];
+                }
+                if(empty($this->Request->post['customer-last-name'])) {
+                    $error = true;
+                    $messages[] = $this->Language->get("error_last_name_empty");
+                }else if($customer['last_name'] != $this->Request->post['customer-last-name']) {
+                    $data['last_name'] = $this->Request->post['customer-last-name'];
+                }
+                if(empty($this->Request->post['customer-mobile'])) {
+                    $error = true;
+                    $messages[] = $this->Language->get("error_mobile_empty");
+                }else if($customer['mobile'] != $this->Request->post['customer-mobile']) {
+                    $data['mobile'] = $this->Request->post['customer-mobile'];
+                }
+                $json = [];
+                if(!$error) {
+                    $Customer->edit($customer_id, $data);
+                    $json['status'] = 1;
+                    $json['messages'] = [$this->Language->get('message_success_done')];
+                    $json['redirect'] = URL . 'user/index?token=' . $_SESSION['token'];
+                }
+                if($error) {
+                    $json['status'] = 0;
+                    $json['messages'] = $messages;
+                }
+                $this->Response->setOutPut(json_encode($json));
+                return;
+            }else {
+                $data['Customer'] = $customer;
+                $this->Response->setOutPut($this->render('user/edit', $data));
+                return;
+            }
+        }
+        return new Action('error/notFound', 'web');
     }
 
 }
